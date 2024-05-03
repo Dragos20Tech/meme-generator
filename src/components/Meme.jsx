435d66e3c6f167f203a587f82ff6053358582abb
Dragos20Tech <dragospetrescu5@gmail.com>
2024-05-03 11:19:27 +0300

    Simulated an API call and removed form tag since we have no data to submit

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,33 +1,32 @@
 import React from "react"
+import memesData from "../memesData.js"
 
 export default function Meme() {
+
+    function handleURL() {
+        const randomNumber = Math.floor(Math.random() * 100) + 1;
+        console.log(memesData.data.memes[randomNumber].url)
+    }
     return (
         <main>
-            <form className="form">
-                <div>
-                    <label>Top Text
-                        <input
-                            type="text"
-                            placeholder="Top text"
-                            className="form--input"
-                        />
-                    </label>
-                </div>
-                <div>
-                    <label>Bottom Text
-                        <input
-                            type="text"
-                            placeholder="Bottom text"
-                            className="form--input"
-                        />
-                    </label>
-                </div>
+            <div className="form">
+                <input
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
                 <button
                     className="form--button"
+                    onClick={handleURL}
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
         </main>
     )
 }
